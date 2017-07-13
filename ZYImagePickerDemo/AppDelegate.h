90f9bb477f527527ed4e37a59789ec3d464f54c8
Daniel <1113135372@qq.com>
2017-07-13 11:18:35 +0800

    Initial Commit

---
+++ ZYImagePickerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZYImagePickerDemo
+//
+//  Created by WeiLuezh on 2017/7/13.
+//  Copyright © 2017年 Daniel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZYImagePickerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZYImagePickerDemo
+//
+//  Created by WeiLuezh on 2017/7/13.
+//  Copyright © 2017年 Daniel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
